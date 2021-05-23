bc314bf439b3f2b8c4931efdb1faf57bb7168d69
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2021-05-23 18:11:00 +0200

    Lodash Yargs

--- basico/lodash_yargs/app.js
+++ basico/lodash_yargs/app.js
@@ -1,15 +1,48 @@
 const _ = require('lodash');
+const argv = require('yargs').argv;
 
-let x = { 'nombre': 'Jaime' };
-let y = { 'apodo': 'MitoCode' };
-let z = [
-	{ nombre: 'Jaime', apellido: 'Medina', edad: 26 },
-	{ nombre: 'Mito', apellido: 'Code', edad: 26 },
-];
+// if (comando === 'Usuario') {
+// 	if (process.argv[3] === 'MitoCode') {
+// 		let x = { 'nombre': 'Jaime' };
+// 		let y = { 'apodo': 'MitoCode' };
+// 		let z = [
+// 			{ nombre: 'Jaime', apellido: 'Medina', edad: 26 },
+// 			{ nombre: 'Mito', apellido: 'Code', edad: 26 },
+// 		];
 
-//UNIR DOS OBJETOS
-// let resultado = _.assign(x, y);
-// console.log(resultado);
+// 		//UNIR DOS OBJETOS
+// 		// let resultado = _.assign(x, y);
+// 		// console.log(resultado);
 
-//
-_.times(3, () => console.log('Subscribete'));
+// 		//EJECUTAR UNA FUNCION VARIAS VECES
+// 		// _.times(3, () => console.log('Subscribete'));
+
+// 		//ENCONTRAR OBJETOS DENTRO DE UNA COLECCION DE OBJETOS
+// 		let resultado = _.find(z, { apellido: 'Code' });
+// 		console.log(resultado);
+// 	} else {
+// 		console.log('Usuario no válido');
+// 	}
+// }
+
+if (argv.usuario === 'MitoCode') {
+	let x = { 'nombre': 'Jaime' };
+	let y = { 'apodo': 'MitoCode' };
+	let z = [
+		{ nombre: 'Jaime', apellido: 'Medina', edad: 26 },
+		{ nombre: 'Mito', apellido: 'Code', edad: 26 },
+	];
+
+	//UNIR DOS OBJETOS
+	// let resultado = _.assign(x, y);
+	// console.log(resultado);
+
+	//EJECUTAR UNA FUNCION VARIAS VECES
+	// _.times(3, () => console.log('Subscribete'));
+
+	//ENCONTRAR OBJETOS DENTRO DE UNA COLECCION DE OBJETOS
+	let resultado = _.find(z, { apellido: 'Code' });
+	console.log(resultado);
+} else {
+	console.log('Usuario no válido');
+}
